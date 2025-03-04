588fc0fe4e85395144d6bf4cc0d2be4f3783452a
aleshanz <yulianto.kitech@gmail.com>
2025-03-04 10:00:28 +0700

    tampilkan nama user dari db

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -70,9 +70,10 @@
             <a href="#" class="navbar-link dropdown-toggle d-flex align-items-center" data-bs-toggle="dropdown">
                 <img src="/assets/img/user/user-13.jpg" alt="" /> 
                 <span>
-                    <span class="d-none d-md-inline">Adam Schwartz</span>
-                    <b class="caret"></b>
-                </span>
+    <span class="d-none d-md-inline">{{ auth()->user()->name }}</span>
+    <b class="caret"></b>
+</span>
+
             </a>
             @include('includes.component.header-dropdown-profile')
         </div>
